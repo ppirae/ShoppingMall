64475b7074472cdd0c69b1646f8777455b38a7b5
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-08-01 11:56:10 +0900

    spring-boot-devtools 설정

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -1,7 +1,6 @@
 package com.shop.repository;
 
 import com.shop.entity.Item;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
